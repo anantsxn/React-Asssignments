d66bdc49b9bab102996c087daf5b1ca70c48dfae
Anant Saxena <anant.saxena@objectedge.com>
2019-11-18 16:21:36 +0530

    Card CSS done; Carousel under works

--- src/components/Home Page/carousel/carousel.js
+++ src/components/Home Page/carousel/carousel.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import Card from '../card/card';
+import React, { Component } from "react";
+import Card from "../card/card";
 
 class Carousel extends Component {
   constructor(props) {
@@ -7,13 +7,45 @@ class Carousel extends Component {
     this.state = {};
   }
 
-  render() {
+  leftShift = () => {
+    if (this.left < 0) this.left = this.left + 300;
+    this.list.style.left = `${this.left}px`;
+  };
+
+  // rightShift=()=>{
+  //   if(this.left > )
+  // }
+
+  listProducts() {
     const { products } = this.props;
+    let tempListProducts = products.map(product => {
+      return <Card key={product.sfid} product={product} />;
+    });
+    return tempListProducts;
+  }
+
+  render() {
+    const products = this.listProducts();
     return (
-      <div>
-        {products.map(product => {
-          return <Card key={product.sfid} product={product} />;
-        })}
+      <div className="carousel-css">
+        <div className="btn left" onClick={this.leftShift}>
+          {" "}
+          &#10094;
+        </div>
+        <div className="btn right" onClick={this.rightShift}>
+          {" "}
+          &#10095;{" "}
+        </div>
+        <div className="carouselCards">
+          <div
+            className="listOfCards"
+            ref={ref => {
+              this.list = ref;
+            }}
+          >
+            {products}
+          </div>
+        </div>
       </div>
     );
   }
